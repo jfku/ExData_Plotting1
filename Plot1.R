647edb3469f96486eb28c5e36072b9d64703a0c1
jfku <30553709+jfku@users.noreply.github.com>
2017-09-19 20:55:24 +0200

    Plots and scripts

---
+++ Plot1.R
@@ -0,0 +1,30 @@
+# Load package required
+library(dplyr)
+
+# Set working directory
+setwd("C:/Users/jfku/Documents")
+
+# Unzip the downloaded file and store the unzipped file in specific folder
+unzip("C:/Users/jfku/Documents/exdata_data_household_power_consumption.zip",exdir = "data_household_power_consumption")
+
+# Read data into R
+data <- read.table("data_household_power_consumption/household_power_consumption.txt", sep = ";", header = TRUE, stringsAsFactors = FALSE, na.strings = "?")
+
+# reformat the columns with dates (character) to Date format
+data$Date <- as.Date(data$Date,format="%d/%m/%Y")
+
+# Make new column with concatenation of Date & Time 
+data$DateTime <- paste(data$Date, data$Time, sep = " ")
+
+# Format new DateTime column 
+data$DateTime <- strptime(data$DateTime, format="%Y-%m-%d %H:%M:%S")
+data$DateTime <- as.POSIXct(data$DateTime)
+
+# subset with only data from the two dates in Feb 2007
+data_feb <- subset(data, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+
+# Make plot1 and print it as png file with 480*480 format
+png("plot1.png", 480, 480)
+plot1 <- hist(data_feb$Global_active_power, main = "Global Active Power"
+              ,xlab = "Global Active Power (Kilowatts)",ylab = "Frequency", col = "red")
+dev.off()
